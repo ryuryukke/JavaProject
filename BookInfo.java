0ec391da63f94664c55d01f87f3233c4e31ee9e5
ryuryukke <ryuto-alexander-koike@keio.jp>
2020-05-14 08:18:37 +0900

    first commit

---
+++ BookInfo.java
@@ -0,0 +1,11 @@
+public class BookInfo
+{
+    String i_title,i_ISBN,i_author,i_publisher,i_pyear; 
+    
+    public BookInfo(String title, String ISBN, String author, String publisher, String pyear){
+	this.i_title = title; this.i_ISBN = ISBN;
+	this.i_author = author; this.i_publisher = publisher;
+	this.i_pyear = pyear;
+    }
+    
+}
